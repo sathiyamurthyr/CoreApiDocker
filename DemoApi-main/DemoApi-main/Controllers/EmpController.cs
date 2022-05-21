cb2e289c43a001d9f95349d16d28397b6d0d80d3
Sathiya Murthy <73695359+sathiyamurthyr@users.noreply.github.com>
2022-05-21 20:08:28 +0530

    First time add the files

---
+++ DemoApi-main/DemoApi-main/Controllers/EmpController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DemoApi.Controllers
+{
+    [Route("api/cmp")]
+    [ApiController]
+    public class EmpController : ControllerBase
+    {
+        [HttpGet]
+        public string EmpList()
+        {
+            return "Sathiya-1";
+        }
+    }
+}
